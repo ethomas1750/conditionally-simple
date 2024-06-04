9def26cda54fb79dd6dcff8058dcf9054a965ba6
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-04 13:37:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,42 +5,40 @@ function isItLong(str){
 if(str.length > 20){
   return "That's a long string!"
 }
-}
 
-function isItMedium(str){
-if(str.length >= 10 && str.length <= 20){
-  return "That's a regular sized string!"
-}
-}
-
-function isItShort(str){
-if(str.length < 10){
-  return "That's a small string!"
-} else{
-  return "That's not a small string!"
-}
-}
-
-function howLongIsMyString(str){
-  if(str.length > 20){
-    return "That's a long string!"
-  } if(str.length >= 10 && str.length <= 20){
-return "That's a regular sized string!"
-  } else if(str.length < 10){
-return "That's a small string!"
+ function isItMedium(str){
+  if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
   }
 }
+  
+function isItShort(str){
+  if (str.length < 10){
+    return "That's a small string!"
+  } else {
+    return "That's not a small string!"
+  }
+} 
+  
+ function howLongIsMyString(str){
+  if (str.length > 20){
+    return "That's a long string!"
+  } else if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  } else if (str.length < 10) {
+    return "That's a small string!"
+  }
+} 
 
-function personsHeight(str){
-if(str === "Shaq"){
-  return 85
-} if(str === "Zooey"){
-  return 65
-}else {
-  return "I don't know everyone's height!"
-}
-}
-
+ function personHeight(str){
+  if(str === "Shaq"){
+    return 85
+  } else if(str === "Zooey"){
+    return 65
+  } else{
+    return "I don't know everyone's height"
+  }
+ }
 /**********************************
  *      OUR CODE GOES BELOW.      *
  * THIS IS FOR INTERNAL USE ONLY. *
